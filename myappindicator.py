9a8a5f5a47e510f257f5be8410da0590cf4ab8fd
Towsif Ahmed Omi <towsif031@gmail.com>
2020-03-16 12:30:10 +0600

    init indicator

---
+++ myappindicator.py
@@ -0,0 +1,58 @@
+# This code is an example for a tutorial on Ubuntu Unity/Gnome AppIndicators:
+# http://candidtim.github.io/appindicator/2014/09/13/ubuntu-appindicator-step-by-step.html
+
+import os
+import gi
+import signal
+import json
+
+from urllib import request
+from urllib.error import URLError
+from urllib.request import urlopen
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
+gi.require_version('Notify', '0.7')
+
+from gi.repository import Gtk as gtk
+from gi.repository import AppIndicator3 as appindicator
+from gi.repository import Notify as notify
+
+
+APPINDICATOR_ID = 'myappindicator'
+
+def main():
+    indicator = appindicator.Indicator.new("customtray", "semi-starred-symbolic", appindicator.IndicatorCategory.APPLICATION_STATUS)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(build_menu())
+    notify.init(APPINDICATOR_ID)
+    gtk.main()
+
+def build_menu():
+    menu = gtk.Menu()
+    item_joke = gtk.MenuItem('Joke')
+    item_joke.connect('activate', joke)
+    menu.append(item_joke)
+    item_quit = gtk.MenuItem('Quit')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+
+def fetch_joke():
+    with urlopen('http://api.icndb.com/jokes/random?limitTo=[nerdy]') as req:
+        msg = req.read();
+        msg = msg.decode('utf-8')
+        joke = json.loads(msg)['value']['joke']
+    return joke
+
+def joke(_):
+    notify.Notification.new("<b>Joke</b>", fetch_joke(), None).show()
+
+def quit(_):
+    notify.uninit()
+    gtk.main_quit()
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    main()
